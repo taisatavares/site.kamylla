74ff9abec96038b4f9bc5cdfdef906925165cad1
taisatavares <taisa.tavares93@gmail.com>
2024-04-16 17:57:10 -0300

    alteração de foto

--- scripts.js
+++ scripts.js
@@ -6,3 +6,26 @@ function toggleMenu(){
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+const button = document.querySelector('.btn-servicos');
+
+const modal = document.querySelector('.dialog');
+
+const buttonClose = document.querySelector('.close');
+
+
+console.log(button);
+
+/*button.forEach(btn => {
+    btn.onclick = function() {
+        alert(btn.value)
+    }
+})*/
+
+button.onclick = function () {
+    modal.show()
+}
+
+buttonClose.onclick = function () {
+    modal.close()
+}
